509e83419cad6c2586090872ef27053007161d67
Kairos22 <alandiko84@gmail.com>
2022-07-13 18:52:29 +0200

    Exercise1

---
+++ solution.js
@@ -0,0 +1,42 @@
+// 1 multiplier
+function multiply(a, b) {
+return a * b;
+};
+ console.log(multiply(3 , 5));
+
+console.log('-------------------------------1');
+
+// 2
+const secondQues = function multiplier2(a, b) {
+   return a * b;
+};
+console.log(secondQues(1, 2));
+
+console.log('------------------------------2');
+
+// 3 Arrow Function
+
+let thirdQues = (a, b) => {
+     return a * b; 
+};
+ console.log(thirdQues(7, 8));
+
+console.log('------------------------------3');
+
+// 4
+function remainder (a,b){
+    console.log(a % b);
+}
+remainder (7, 3)
+
+const remainder1 = function (a,b) {
+    return a % b ;
+}
+console.log(remainder1(7,5));
+
+const remainder2 = (a, b) => {
+console.log(a % b);
+};
+
+remainder2(6, 4); 
+console.log('-------------------------------4');
